6d0c624549027f3c77ed1e9476a84676a6e83d7d
Shrewdkenny <shrewdkenny@gmail.com>
2024-06-22 18:51:44 +0100

    updated

--- src/views/PricingPage.vue
+++ src/views/PricingPage.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="bg-[#ffffff] px-5 py-5 lg:px-28 lg:py-10">
     <HomePageHeader />
-    <div class="mt-20 flex flex-col items-center justify-center gap-20">
-      <!-- data-aos="fade-left" -->
+    <div
+      class="mt-20 flex flex-col items-center justify-center gap-20"
+      data-aos="fade-left"
+    >
       <h1
         class="rounded-lg border-2 border-[#c5c7cd] bg-[#fcfcfc] px-48 py-3 text-xl font-normal"
       >
