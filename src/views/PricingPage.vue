85f7b860a3cacd6f329406c2fc928e8b7364fd6b
Shrewdkenny <shrewdkenny@gmail.com>
2024-06-22 18:48:34 +0100

    updated

--- src/views/PricingPage.vue
+++ src/views/PricingPage.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="bg-[#ffffff] px-5 py-5 lg:px-28 lg:py-10">
     <HomePageHeader />
-    <div data-aos="fade-left">pricing page</div>
+    <div class="mt-20 flex flex-col items-center justify-center gap-20">
+      <!-- data-aos="fade-left" -->
+      <h1
+        class="rounded-lg border-2 border-[#c5c7cd] bg-[#fcfcfc] px-48 py-3 text-xl font-normal"
+      >
+        Calculate Dispatch Cost
+      </h1>
+      <div></div>
+    </div>
   </div>
 </template>
 
