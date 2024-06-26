d0b3aa8b0d3bdf3aa86a54678c08dcfb6c2ce38f
Shrewdkenny <shrewdkenny@gmail.com>
2024-06-26 15:50:13 +0100

    updated

--- src/components/DashBoardCards.vue
+++ src/components/DashBoardCards.vue
@@ -6,7 +6,8 @@
       class="flex h-40 flex-col items-center justify-center rounded-lg bg-[#ffffff]"
     >
       <h1 class="text-lg font-normal text-[#545c63]">create new dispatch</h1>
-      <svg
+      <img src="@/assets/images/shipping.svg" alt="" class="w-14" />
+      <!-- <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
@@ -20,13 +21,14 @@
         <path
           d="M3 18.4v-2.796a4.3 4.3 0 0 0 .713.31A26.226 26.226 0 0 0 12 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 0 1-6.477-.427C4.047 21.128 3 19.852 3 18.4Z"
         />
-      </svg>
+      </svg> -->
     </div>
     <div
       class="flex h-40 flex-col items-center justify-center rounded-lg bg-[#ffffff]"
     >
       <h1 class="text-lg font-normal text-[#545c63]">View pending orders</h1>
-      <svg
+      <img src="@/assets/images/pending.svg" alt="" class="w-14" />
+      <!-- <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
@@ -37,14 +39,14 @@
           d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
           clip-rule="evenodd"
         />
-      </svg>
+      </svg> -->
     </div>
     <div
       class="flex h-40 flex-col items-center justify-center rounded-lg bg-[#ffffff]"
     >
-      <!-- class="flex h-[130px] w-full flex-col items-center justify-center rounded-lg bg-[#ffffff] lg:h-[170px] lg:max-w-[400px]" -->
       <h1 class="text-lg font-normal text-[#545c63]">View Order History</h1>
-      <svg
+      <img src="@/assets/images/history.svg" alt="" class="w-14" />
+      <!-- <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -57,13 +59,14 @@
           stroke-linejoin="round"
           d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
         />
-      </svg>
+      </svg> -->
     </div>
     <div
       class="flex h-40 flex-col items-center justify-center rounded-lg bg-[#ffffff]"
     >
       <h1 class="text-lg font-normal text-[#545c63]">Fund Your Wallet</h1>
-      <svg
+      <img src="@/assets/images/wallet.svg" alt="" class="w-14" />
+      <!-- <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -76,7 +79,7 @@
           stroke-linejoin="round"
           d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"
         />
-      </svg>
+      </svg> -->
     </div>
   </div>
 </template>
@@ -85,11 +88,6 @@
 export default {
   name: "Test",
   created() {},
-  data() {
-    return {};
-  },
-  props: {},
-  methods: {},
 };
 </script>
 
