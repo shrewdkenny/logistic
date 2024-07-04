9dd5e9ac8a7e307fd4d509b6d0a8537f928a218e
Shrewdkenny <shrewdkenny@gmail.com>
2024-07-04 17:57:29 +0100

    animate

--- src/components/AsideSmallScreen.vue
+++ src/components/AsideSmallScreen.vue
@@ -1,24 +1,32 @@
 <template>
-  <div
-    class="cursor-pointer flex-col bg-[#022045] duration-300 ease-in pt-10 w-64"
-    v-if="asideStore.isAsideVisible"
+  <transition
+    enter-active-class="transition transform ease-out duration-500"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-active-class="transition transform ease-in duration-300"
+    leave-from-class="translate-x-0"
+    leave-to-class="-translate-x-full"
   >
-    <router-link to="/">
-      <div class="flex justify-center">
-        <img
-          src="@/assets/images/greensexlogo2.svg"
-          alt=""
-          class="w-40"
-        />
-      </div>
-    </router-link>
-    <AsideLinks @click="handleCloseAside" />
-  </div>
+    <div
+      v-if="asideStore.isAsideVisible"
+      class="w-64 cursor-pointer flex-col bg-[#022045] pt-10"
+    >
+      <router-link to="/">
+        <div class="flex justify-center">
+          <img src="@/assets/images/greensexlogo2.svg" alt="" class="w-40" />
+        </div>
+      </router-link>
+      <AsideLinks @click="handleCloseAside" />
+    </div>
+  </transition>
 </template>
 
 <script>
 import { useStore } from "@/stores/store";
 import AsideLinks from "@/components/AsideLinks.vue";
+import { onMounted } from "vue";
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default {
   name: "Test",
   components: {
@@ -30,6 +38,9 @@ export default {
     const handleCloseAside = () => {
       asideStore.closeAside();
     };
+    onMounted(() => {
+      AOS.init();
+    });
     return {
       asideStore,
       handleCloseAside,
